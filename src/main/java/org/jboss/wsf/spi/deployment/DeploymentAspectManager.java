388d38b0f8f860affad02b3e9e56a4cdd876ac69
Thomas Diesler <thomas.diesler@jboss.com>
2007-08-08 09:19:04 +0000

    Use 'Pre' and 'Post' for JSE handling Move notion of parent to DAM instead of DAM intaller Revert container integration version back to snapshot Sort out commented requires, provides clauses

--- src/main/java/org/jboss/wsf/spi/deployment/DeploymentAspectManager.java
+++ src/main/java/org/jboss/wsf/spi/deployment/DeploymentAspectManager.java
@@ -36,6 +36,12 @@ public interface DeploymentAspectManager
    /** Get the name for this aspect manager */
    String getName();
    
+   /** Get the optional parent for this manager */
+   DeploymentAspectManager getParent();
+   
+   /** Set the optional parent for this manager */
+   void setParent(DeploymentAspectManager dam);
+   
    /** Get the ordered list of registered deployment aspects */
    List<DeploymentAspect> getDeploymentAspects();
    
