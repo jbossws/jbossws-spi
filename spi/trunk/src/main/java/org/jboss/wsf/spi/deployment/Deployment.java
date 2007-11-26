ef6860529c3d2f71f12e5c5b2f3cd1fe1bb5c814
heiko.braun@jboss.com <heiko.braun@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2007-11-26 17:38:22 +0000

    Rollback

--- spi/trunk/src/main/java/org/jboss/wsf/spi/deployment/Deployment.java
+++ spi/trunk/src/main/java/org/jboss/wsf/spi/deployment/Deployment.java
@@ -25,18 +25,18 @@ package org.jboss.wsf.spi.deployment;
 // $Id$
 
 /**
- * A general web service deployment dep. 
- * 
- * It has no notion of J2EE deployment packages. 
- * 
+ * A general web service deployment dep.
+ *
+ * It has no notion of J2EE deployment packages.
+ *
  * @author Thomas.Diesler@jboss.com
- * @since 20-Apr-2007 
+ * @since 20-Apr-2007
  */
 public interface Deployment extends Extensible
 {
    public enum DeploymentType
    {
-      JAXRPC_CLIENT, JAXRPC_JSE, JAXRPC_EJB21, JAXRPC_EJB3, JAXWS_JSE, JAXWS_EJB3, REST_JSE, REST_EJB3
+      JAXRPC_CLIENT, JAXRPC_JSE, JAXRPC_EJB21, JAXRPC_EJB3, JAXWS_JSE, JAXWS_EJB3
    };
    
    public enum DeploymentState
