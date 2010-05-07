2b914792b64ac98e57c69cbf9e57d58dcbfffc34
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2010-05-07 11:20:14 +0000

    [JBPAPP-4124] Move to Apache CXF 2.2.5 (merge revision 11130)

--- spi/branches/jbossws-spi-1.1.2/src/main/java/org/jboss/wsf/spi/deployment/ArchiveDeployment.java
+++ spi/branches/jbossws-spi-1.1.2/src/main/java/org/jboss/wsf/spi/deployment/ArchiveDeployment.java
@@ -50,7 +50,10 @@ public interface ArchiveDeployment extends Deployment
    /** The concatenated names including all parents. */
    String getCanonicalName();
    
-   /** Get the URL for a given resource path */
+   /** Get the URL for a given resource path
+    *  Deprecated, use getResourceResolver().resolve(String resourcePath).
+    */
+   @Deprecated
    URL getMetaDataFileURL(String resourcePath) throws IOException;
    
    /** Get additional metadata files attached to the deployment **/
@@ -58,4 +61,8 @@ public interface ArchiveDeployment extends Deployment
    
    /** Set additional metadata files for this deployment **/
    void setMetadataFiles(List<UnifiedVirtualFile> metadataFiles);
+   
+   /** Get the resource resolver to be used to access resources from this archive */
+   ResourceResolver getResourceResolver();
+   
 }
