13837a3c36f7afc7891137496bbbc84faad230cd
Alessio Soldano <asoldano@users.noreply.github.com>
2011-05-18 08:41:29 +0000

    [JBWS-3296] Providing SecurityDomainContext interface to abstract picketbox/jboss-security AuthenticationManager stuff

--- src/main/java/org/jboss/wsf/spi/deployment/Endpoint.java
+++ src/main/java/org/jboss/wsf/spi/deployment/Endpoint.java
@@ -31,6 +31,7 @@ import org.jboss.ws.api.monitoring.RecordProcessor;
 import org.jboss.wsf.spi.invocation.InvocationHandler;
 import org.jboss.wsf.spi.invocation.RequestHandler;
 import org.jboss.wsf.spi.management.EndpointMetrics;
+import org.jboss.wsf.spi.security.SecurityDomainContext;
 
 /**
  * A general JAXWS endpoint.
@@ -140,4 +141,10 @@ public interface Endpoint extends Extensible
 
    /** Set endpoint address */
    void setAddress(String address);
+   
+   /** Get security domain context */
+   SecurityDomainContext getSecurityDomainContext();
+   
+   /** Set security domain context */
+   void setSecurityDomainContext(SecurityDomainContext context);
 }
