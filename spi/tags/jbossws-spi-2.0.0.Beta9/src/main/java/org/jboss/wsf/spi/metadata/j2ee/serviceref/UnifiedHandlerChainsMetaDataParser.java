baf99f683fec072011a6affd46ac7639e5aedb22
richard.opalka@jboss.com <richard.opalka@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2011-09-12 12:28:08 +0000

    tagging JBossWS SPI

---
+++ spi/tags/jbossws-spi-2.0.0.Beta9/src/main/java/org/jboss/wsf/spi/metadata/j2ee/serviceref/UnifiedHandlerChainsMetaDataParser.java
@@ -0,0 +1,104 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.wsf.spi.metadata.j2ee.serviceref;
+
+import static javax.xml.stream.XMLStreamConstants.END_ELEMENT;
+import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
+import static org.jboss.wsf.spi.metadata.ParserConstants.HANDLER_CHAINS;
+import static org.jboss.wsf.spi.metadata.ParserConstants.J2EE_NS;
+import static org.jboss.wsf.spi.metadata.ParserConstants.JAVAEE_NS;
+import static org.jboss.wsf.spi.util.StAXUtils.match;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ResourceBundle;
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+
+import org.jboss.ws.api.util.BundleUtils;
+import org.jboss.wsf.spi.metadata.AbstractHandlerChainsMetaDataParser;
+import org.jboss.wsf.spi.util.StAXUtils;
+
+/**
+ * The parser for the unified metadata handler chains element
+ * 
+ * @author alessio.soldano@jboss.com
+ * @since 26-Nov-2010
+ */
+public class UnifiedHandlerChainsMetaDataParser extends AbstractHandlerChainsMetaDataParser
+{
+   private static final ResourceBundle bundle = BundleUtils.getBundle(UnifiedHandlerChainsMetaDataParser.class);
+   private UnifiedHandlerChainsMetaDataParser()
+   {
+      super();
+   }
+   
+   public static UnifiedHandlerChainsMetaData parse(InputStream is) throws IOException
+   {
+      // http://java.sun.com/xml/ns/javaee/javaee_web_services_1_2.xsd
+      try
+      {
+         XMLStreamReader xmlr = StAXUtils.createXMLStreamReader(is);
+         return parse(xmlr);
+      }
+      catch (XMLStreamException e)
+      {
+         throw new IOException(e);
+      }
+   }
+   
+   public static UnifiedHandlerChainsMetaData parse(XMLStreamReader reader) throws XMLStreamException
+   {
+      int iterate;
+      try
+      {
+         iterate = reader.nextTag();
+      }
+      catch (XMLStreamException e)
+      {
+         // skip non-tag elements
+         iterate = reader.nextTag();
+      }
+      UnifiedHandlerChainsMetaData handlerChains = null;
+      switch (iterate)
+      {
+         case END_ELEMENT : {
+            // we're done
+            break;
+         }
+         case START_ELEMENT : {
+
+            if (match(reader, JAVAEE_NS, HANDLER_CHAINS) || match(reader, J2EE_NS, HANDLER_CHAINS))
+            {
+               UnifiedHandlerChainsMetaDataParser parser = new UnifiedHandlerChainsMetaDataParser();
+               handlerChains = parser.parseHandlerChains(reader, reader.getNamespaceURI());
+            }
+            else
+            {
+               throw new IllegalStateException(BundleUtils.getMessage(bundle, "UNEXPECTED_ELEMENT",  reader.getLocalName()));
+            }
+         }
+      }
+      return handlerChains;
+   }
+}
