7ec772db5b89b37ce8a25edcd0f089eb881eea4a
jim.ma <jim.ma@50344849-fb0f-0410-a53f-a94597aac05c>
2011-09-13 04:53:30 +0000

    Remove temp worksapce

--- spi/branches/JBWS-3343/src/main/java/org/jboss/wsf/spi/deployment/EndpointTypeFilter.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.wsf.spi.deployment;
-
-import org.jboss.wsf.spi.deployment.Endpoint.EndpointType;
-
-public interface EndpointTypeFilter
-{
-   boolean accept(EndpointType type);
-
-}
--- spi/branches/JBWS-3343/src/main/java/org/jboss/wsf/spi/management/WebServerInfo.java
+++
@@ -1,6 +0,0 @@
-package org.jboss.wsf.spi.management;
-
-public interface WebServerInfo
-{
-   int getPort(String protocol, boolean secure);
-}
