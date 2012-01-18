1219ef76b876171c9acf98c81535c74967d1fe65
alessio.soldano@jboss.com <alessio.soldano@jboss.com@50344849-fb0f-0410-a53f-a94597aac05c>
2012-01-18 13:14:06 +0000

    [AS7-3324] Adding method using WebServicesMetadata in EndpointPublisher

--- spi/trunk/src/main/java/org/jboss/wsf/spi/publish/EndpointPublisher.java
+++ spi/trunk/src/main/java/org/jboss/wsf/spi/publish/EndpointPublisher.java
@@ -20,6 +20,8 @@ package org.jboss.wsf.spi.publish;
 
 import java.util.Map;
 
+import org.jboss.wsf.spi.metadata.webservices.WebservicesMetaData;
+
 /**
  * EndpointPublisher defines the interface for facilities allowing to publish/destroy
  * WS endpoints on top of the running JBoss Application Server container.
@@ -29,6 +31,8 @@ import java.util.Map;
 public interface EndpointPublisher
 {
    public Context publish(String contextRoot, ClassLoader loader, Map<String, String> urlPatternToClassNameMap) throws Exception;
+   
+   public Context publish(String contextRoot, ClassLoader loader, Map<String, String> urlPatternToClassNameMap, WebservicesMetaData metadata) throws Exception;
    
    public void destroy(Context context) throws Exception;
 }
