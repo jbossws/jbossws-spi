fce5bef3624a7e3234425aa6d192c03996e188f8
Alessio Soldano <asoldano@users.noreply.github.com>
2012-02-03 11:38:52 +0000

    [AS7-3581] Adding interfaces for allowing setting AS7 NamespaceContextSelector from CXF stack

--- src/main/java/org/jboss/wsf/spi/invocation/NamespaceContextSelectorWrapper.java
+++ src/main/java/org/jboss/wsf/spi/invocation/NamespaceContextSelectorWrapper.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source.
- * Copyright 2006, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2012, Red Hat Middleware LLC, and individual contributors
  * as indicated by the @author tags. See the copyright.txt file in the
  * distribution for a full listing of individual contributors.
  *
@@ -21,30 +21,21 @@
  */
 package org.jboss.wsf.spi.invocation;
 
-import org.jboss.wsf.spi.deployment.Endpoint;
+import java.util.Map;
 
 /**
- * Associates the endpoint meta data with the current thread. 
- * 
- * @author Thomas.Diesler@jboss.org
- * @since 10-May-2007
+ * Wraps AS7 NamespaceContextSelector logic to allow carrying its
+ * contents for invocation served using thread pools.
+ *
+ * @author alessio.soldano@jboss.org
+ * @since 03-Feb-2012
+ *
  */
-public final class EndpointAssociation
+public interface NamespaceContextSelectorWrapper
 {
-   private static final ThreadLocal<Endpoint> endpoint = new ThreadLocal<Endpoint>();
-
-   public static void setEndpoint(Endpoint ep)
-   {
-      endpoint.set(ep);
-   }
-
-   public static Endpoint getEndpoint()
-   {
-      return endpoint.get();
-   }
-
-   public static void removeEndpoint()
-   {
-      endpoint.remove();
-   }
+   public void storeCurrentThreadSelector(Map<String, Object> map);
+   
+   public void setCurrentThreadSelector(Map<String, Object> map);
+   
+   public void clearCurrentThreadSelector(Map<String, Object> map);
 }
