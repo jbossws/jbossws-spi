a3a73d86a088b846676dd2f7f1f9a3ce9b7ef634
Alessio Soldano <asoldano@users.noreply.github.com>
2014-05-13 20:20:00 +0000

    [JBWS-3793] More changes to j2ee metadata

--- src/main/java/org/jboss/wsf/spi/metadata/j2ee/JSEArchiveMetaData.java
+++ src/main/java/org/jboss/wsf/spi/metadata/j2ee/JSEArchiveMetaData.java
@@ -21,6 +21,7 @@
  */
 package org.jboss.wsf.spi.metadata.j2ee;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -34,30 +35,134 @@ import java.util.Map;
 public class JSEArchiveMetaData
 {
    // The war context root as specified at the jboss-web.xml descriptor level.
-   private String contextRoot;
+   private final String contextRoot;
    // The servlet-mapping <servlet-name, url-pattern> 
-   private Map<String, String> servletMappings = new HashMap<String, String>();
+   private final Map<String, String> servletMappings;
    // The servlet <servlet-name, servlet-class> 
-   private Map<String, String> servletClassNames = new HashMap<String, String>();
+   private final Map<String, String> servletClassNames;
    // The optional JBossWS config-name 
-   private String configName;
+   private final String configName;
    // The optional JBossWS config-file 
-   private String configFile;
+   private final String configFile;
    // The security-domain value assigned to the application 
-   private String securityDomain;
+   private final String securityDomain;
    // A HashMap<String, String> for webservice description publish locations 
-   private PublishLocationAdapter publishLocationAdapter;
+   private final PublishLocationAdapter publishLocationAdapter;
    // web.xml security-constraints 
-   private List<JSESecurityMetaData> securityMetaData;
+   private final List<JSESecurityMetaData> securityMetaData;
+   
+   public static class Builder
+   {
+      private String contextRoot;
+      private Map<String, String> servletMappings = new HashMap<String, String>();
+      private Map<String, String> servletClassNames = new HashMap<String, String>();
+      private String configName;
+      private String configFile;
+      private String securityDomain;
+      private PublishLocationAdapter publishLocationAdapter;
+      private List<JSESecurityMetaData> securityMetaData;
+      
+      public JSEArchiveMetaData build()
+      {
+         return new JSEArchiveMetaData(contextRoot, servletMappings, servletClassNames, configName, configFile,
+               securityDomain, publishLocationAdapter, securityMetaData);
+      }
+      
+      public String getContextRoot()
+      {
+         return contextRoot;
+      }
+      public void setContextRoot(String contextRoot)
+      {
+         this.contextRoot = contextRoot;
+      }
+      public Map<String, String> getServletMappings()
+      {
+         return servletMappings;
+      }
+      public void setServletMappings(Map<String, String> servletMappings)
+      {
+         this.servletMappings = servletMappings;
+      }
+      public Map<String, String> getServletClassNames()
+      {
+         return servletClassNames;
+      }
+      public void setServletClassNames(Map<String, String> servletClassNames)
+      {
+         this.servletClassNames = servletClassNames;
+      }
+      public String getConfigName()
+      {
+         return configName;
+      }
+      public void setConfigName(String configName)
+      {
+         this.configName = configName;
+      }
+      public String getConfigFile()
+      {
+         return configFile;
+      }
+      public void setConfigFile(String configFile)
+      {
+         this.configFile = configFile;
+      }
+      public String getSecurityDomain()
+      {
+         return securityDomain;
+      }
+      public void setSecurityDomain(String securityDomain)
+      {
+         this.securityDomain = securityDomain;
+      }
+      public PublishLocationAdapter getPublishLocationAdapter()
+      {
+         return publishLocationAdapter;
+      }
+      public void setPublishLocationAdapter(PublishLocationAdapter publishLocationAdapter)
+      {
+         this.publishLocationAdapter = publishLocationAdapter;
+      }
+      public List<JSESecurityMetaData> getSecurityMetaData()
+      {
+         return securityMetaData;
+      }
+      public void setSecurityMetaData(List<JSESecurityMetaData> securityMetaData)
+      {
+         this.securityMetaData = securityMetaData;
+      }
+   }
+   
+   protected JSEArchiveMetaData(String contextRoot, Map<String, String> servletMappings,
+         Map<String, String> servletClassNames, String configName, String configFile, String securityDomain,
+         PublishLocationAdapter publishLocationAdapter, List<JSESecurityMetaData> securityMetaData)
+   {
+      this.contextRoot = contextRoot;
+      if (servletMappings != null) {
+         this.servletMappings = Collections.unmodifiableMap(servletMappings);
+      } else {
+         this.servletMappings = Collections.emptyMap();
+      }
+      if (servletClassNames != null) {
+         this.servletClassNames = Collections.unmodifiableMap(servletClassNames);
+      } else {
+         this.servletClassNames = Collections.emptyMap();
+      }
+      this.configName = configName;
+      this.configFile = configFile;
+      this.securityDomain = securityDomain;
+      this.publishLocationAdapter = publishLocationAdapter;
+      if (securityMetaData != null) {
+         this.securityMetaData = Collections.unmodifiableList(securityMetaData);
+      } else {
+         this.securityMetaData = Collections.emptyList();
+      }
+   }
 
    public String getContextRoot()
    {
       return contextRoot;
-   }
-
-   public void setContextRoot(String contextRoot)
-   {
-      this.contextRoot = contextRoot;
    }
 
    public Map<String, String> getServletMappings()
@@ -65,19 +170,9 @@ public class JSEArchiveMetaData
       return servletMappings;
    }
 
-   public void setServletMappings(Map<String, String> servletMappings)
-   {
-      this.servletMappings = servletMappings;
-   }
-
    public Map<String, String> getServletClassNames()
    {
       return servletClassNames;
-   }
-
-   public void setServletClassNames(Map<String, String> servletClassNames)
-   {
-      this.servletClassNames = servletClassNames;
    }
 
    public String getConfigName()
@@ -85,19 +180,9 @@ public class JSEArchiveMetaData
       return configName;
    }
 
-   public void setConfigName(String configName)
-   {
-      this.configName = configName;
-   }
-
    public String getConfigFile()
    {
       return configFile;
-   }
-
-   public void setConfigFile(String configFile)
-   {
-      this.configFile = configFile;
    }
 
    public String getSecurityDomain()
@@ -105,24 +190,9 @@ public class JSEArchiveMetaData
       return securityDomain;
    }
 
-   public void setSecurityDomain(String securityDomain)
-   {
-      this.securityDomain = securityDomain;
-   }
-
    public List<JSESecurityMetaData> getSecurityMetaData()
    {
       return securityMetaData;
-   }
-
-   public void setSecurityMetaData(List<JSESecurityMetaData> securityMetaData)
-   {
-      this.securityMetaData = securityMetaData;
-   }
-
-   public void setPublishLocationAdapter(PublishLocationAdapter publishLocationAdapter)
-   {
-      this.publishLocationAdapter = publishLocationAdapter;
    }
 
    public String getWsdlPublishLocationByName(String name)
